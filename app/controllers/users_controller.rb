2271fa26b7539fa2d89077490dbd895dd7ecd8f9
Dexter Crockett <dex429.dc@gmail.com>
2022-06-02 19:06:55 -0400

    add new, create, and show to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user_id
+            redirect_to_user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+        redirect_if_not logged_in
+        @user = User.find_by_id(params[:id])
     end
 
 
