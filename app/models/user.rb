17d91739c8d265403683006482a49a0e14545858
Dexter Crockett <dex429.dc@gmail.com>
2022-06-03 15:43:29 -0400

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     has_many :notes
     has_many :strain_notes, through: :notes, source: :strain
     has_many :dry_rooms
+
+    has_secure_password
+
+    validates :user_name, :email, presence: true
+    validates :email, uniqueness: true
 end
