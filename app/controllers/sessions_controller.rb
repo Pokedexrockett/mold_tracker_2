eecf36dc05154e5562575b7b5303a77abcb5af36
Dexter Crockett <dex429.dc@gmail.com>
2022-06-06 20:20:58 -0400

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+    def home
+
+    end
+
+    def new
+
+    end
+
+    def create
+        user = User.find_by(employee: params[:user][:employee]
+        if user.try(:authenticate, params[:user][:password]))
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:error] = "Sorry, loging info was incorrect. Please try again."
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.destroy
+        redirect_to '/'
+    end
+
+end
