8f02ef0de18ec4c65168f186073f8d423084069a
Dexter Crockett <dex429.dc@gmail.com>
2022-06-06 20:12:11 -0400

    add sessions login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   resources :dry_rooms
   resources :strains
   resources :users
-
+  
+  get '/' => 'sessions#home'
+  get '/login' => 'sessions#new'
+  post '/login' 'sessions#create'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
