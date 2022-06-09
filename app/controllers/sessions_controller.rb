a00f0d6aa104968c7106902a6b841ee5a6baa92b
Dexter Crockett <dex429.dc@gmail.com>
2022-06-09 19:45:24 -0400

    start adding helper methods for current user/logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:error] = "Sorry, loging info was incorrect. Please try again."
+            flash[:error] = "Sorry, login info was incorrect. Please try again."
             redirect_to login_path
         end
     end
