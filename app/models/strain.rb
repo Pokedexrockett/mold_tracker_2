5ea3ef87fd77595b6a44560c8e835e06f62b7784
Dexter Crockett <dex429.dc@gmail.com>
2022-06-21 22:36:41 -0400

    add methods to Strain model

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -9,6 +9,17 @@ class Strain < ApplicationRecord
 
     scope :order_by_batch_number, -> {left_joins(:notes).group(:id).order('batch_id desc')}
 
-    
+    def self.alpha
+        order(:name)
+    end
+
+    def dry_room_attributes=(attributes)
+        self.dry_room = DryRoom.find_or_create_by(attributes) if !attributes['room_number'].empty?
+        self.dry_room
+    end
+
+    def name_and_room_number
+        "#{name} - #{dry_room.try(:room_number)}"
+    end
 
 end
