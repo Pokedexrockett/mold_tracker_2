17ae912b3fa2302b4afe841c41e0bfa0d69191f9
Dexter Crockett <dex429.dc@gmail.com>
2022-06-21 20:46:48 -0400

    add new, create, show, and index methods to notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,34 @@
 class NotesController < ApplicationController
 
+   def new
+      if @strain = Strain.find_by_id(params[:strain_id])
+         @review = @strain.notes.build
+      else
+         @review = Review.new
+      end
+   end
+
+   def create 
+      @note = Note.new(note_params)
+      @note.user = current_user
+      if @note.save
+         redirect_to note_path(@note)
+      else
+         render:new
+      end
+   end
+
+   def show
+      @note = Note.find_by_id(params[:id])
+   end
+
+   def index
+      if @strain = Strain.find_by_id(params[:strain_id])
+         @notes = @strain.notes
+      else
+         @notes = Note.all
+   end
+
 
    private
    def note_params
